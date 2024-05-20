73d32c2a14f422fb9ebff484ce15c594cbf26a25
Amelia Hutchinson <129813472+s4004254@users.noreply.github.com>
2024-05-20 10:53:29 +1000

    jbkjdsbvbd

--- assignment2/script.js
+++ assignment2/script.js
@@ -4,6 +4,11 @@ const playPauseImg = document.querySelector("#play-pause-img");
 const progressBar = document.querySelector("#progress-bar-fill");
 video.removeAttribute("controls");
 // playPauseBtn.addEventListener("click", togglePlayPause);
+//I adjusted the colour of the pause and play buttons as well 
+//as the mute button to be the same hue as the type. This is aimed 
+//to produce a connection with the different assets. Changing the 
+//colour to stark white aims to bring the attention of the user towards
+// the buttons as it directly contrasts the black background
 video.addEventListener("timeupdate", updateProgressBar);
 function togglePlayPause() {
   if (video.paused || video.ended) {
@@ -22,14 +27,16 @@ function updateProgressBar() {
   const value = (video.currentTime / video.duration) * 100;
   progressBar.style.width = value + "%";
 }
-const muteUnmuteButton = document.querySelector("#mute-unmute-button");
-console.log(muteUnmuteButton)
+const muteUnmuteButton = document.querySelector("#mute-unmute-buttn");
+
 function toggleSound() {
-    if (video.muted){
-        muteUnmuteButton.style.backgroundColour = "#ffffff";
+    if (video.muted) {
         video.muted = false;
+        muteUnmuteButton.querySelector("img").src = "https://icons8.com/icon/reqgj3e1uKBy/audio";
+        muteUnmuteButton.style.backgroundColor = "#ffffff";
     } else {
-        muteUnmuteButton.style.backgroundColour = "#a5a5a5";
         video.muted = true;
+        muteUnmuteButton.querySelector("img").src = "https://img.icons8.com/ios-glyphs/30/no-audio--v1.png";
+        muteUnmuteButton.style.backgroundColor = "#a5a5a5";
     }
 }
