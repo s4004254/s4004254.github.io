6b2824bb4c73ad172c71c43784da1b8d1b2da9be
Amelia Hutchinson <129813472+s4004254@users.noreply.github.com>
2024-06-19 08:42:05 +1000

    ,jbkjbkbjjk

--- assignment3/script.js
+++ assignment3/script.js
@@ -10,3 +10,13 @@ function increaseLikes() {
   likeCount++;
   likes.textContent = likeCount;
 }
+/* A click action will demonstrate the different characters in Nintendo’s classic games.
+ The user will scroll through images and be able to click on the various images similar
+  to Instagram and give a like. This aims to connect the characters to the user in an 
+  interactive manner. A like is similar to Instagram and other social media when clicking 
+  on a photo to like it and then scrolling to see another image. The user can scroll to 
+  view another image of a character and then like it again. The idea to click is prompted 
+  by the previous knowledge on how to navigate a similar webspace.  This action will allow
+   the user to interact with the website producing a greater connection with the characters
+    and the website. 
+ */
